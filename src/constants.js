4ee2ef0de842f6f546f38d716817e78b07a05b04
Kanishk Singh <kanishk.singh198@gmail.com>
2023-05-28 16:02:21 +0530

    changed stringsToType to have random english words

--- src/constants.js
+++ src/constants.js
@@ -1,25 +1,50 @@
-const stringsToType = [
-  "asd fjk l;",
-  "asdf j; kl",
-  "asd f; jkl",
-  "as; djk l",
-  "asd; fj k",
-  "as d fjk;",
-  "as;df jkl",
-  "asd jkl; f",
-  "as df j; kl",
-  "a; sdfj kl",
-  "a;dfj kls",
-  "as; dj fkl",
-  "as; dfj kl",
-  "asdf klj;",
-  "as; fj kl",
-  "asdfj ;kl",
-  "asd jkl; f",
-  "asf ; jkl",
-  "asdf ;jkl",
-  "asd fkl; j"
+const words = [
+    "cat",
+    "dog",
+    "tree",
+    "book",
+    "bird",
+    "jump",
+    "flower",
+    "happy",
+    "sun",
+    "river",
+    "mountain",
+    "butterfly",
+    "whisper",
+    "courage",
+    "mystery",
+    "chocolate",
+    "adventure",
+    "serendipity",
+    "bedsheet",
+    "cellphone",
+    "computer",
+    "effervescent"
+    // Add more simple words here
 ];
+
+function generateRandomStrings(numStrings) {
+  const randomStrings = [];
+
+  for (let i = 0; i < numStrings; i++) {
+    let word1, word2, word3;
+
+    do {
+      word1 = words[Math.floor(Math.random() * words.length)];
+      word2 = words[Math.floor(Math.random() * words.length)];
+      word3 = words[Math.floor(Math.random() * words.length)];
+    } while (word1 === word2 || word2 === word3 || word1 === word3);
+
+    const randomString = `${word1} ${word2} ${word3}`;
+    randomStrings.push(randomString);
+  }
+
+  return randomStrings;
+}
+
+// Generate 300 random 3-word strings
+const stringsToType = generateRandomStrings(300);
 
 export {
     stringsToType
